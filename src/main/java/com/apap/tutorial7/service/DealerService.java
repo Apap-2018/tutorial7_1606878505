5c78fe972b15ad65445e7d40cd98b458e6a5d265
Zaki Raihan <zaki.raihan98@gmail.com>
2018-10-30 20:06:48 +0700

    initial commit and First Exercise done

---
+++ src/main/java/com/apap/tutorial7/service/DealerService.java
@@ -0,0 +1,26 @@
+package com.apap.tutorial7.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.apap.tutorial7.model.DealerModel;
+
+/**
+ * DealerService
+ * @author Zaki Raihan
+ *
+ */
+public interface DealerService {
+	
+	List<DealerModel> getAllDealer();
+	
+	Optional<DealerModel> getDealerDetailById(Long id);
+	
+	DealerModel addDealer(DealerModel dealer);
+
+	void updateDealer(Long dealerId, DealerModel newDealer);
+
+	void deleteDealerById(Long dealerId);
+
+	void deleteDealer(DealerModel dealer);
+}
